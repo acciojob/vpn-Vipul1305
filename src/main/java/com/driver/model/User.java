177324ff416d8c2cc50e81123142ef55108a4003
unknown <vipulsingh.cg@gmail.com>
2023-04-02 00:01:33 +0530

    sol

--- src/main/java/com/driver/model/User.java
+++ src/main/java/com/driver/model/User.java
@@ -19,7 +19,7 @@ public class User {
 
     private String maskedIp;
 
-    private boolean connected;
+    private Boolean connected;
 
     @ManyToMany(mappedBy = "userList",cascade = CascadeType.ALL)
     List<ServiceProvider> serviceProviderList = new ArrayList<>();
